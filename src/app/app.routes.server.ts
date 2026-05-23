7aa1f526aee9b94dcbe0157cc92306a5a72acde0
Karen B <132723157+ktatianab@users.noreply.github.com>
2026-05-23 14:10:08 -0500

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-  path: 'detalle-evento/:id',
-  renderMode: RenderMode.Server
-  }
+    path: 'detalle-evento/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
 ];
